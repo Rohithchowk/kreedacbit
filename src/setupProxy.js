9342bc1db3ec6dccd7eacb2c98ca015705e1cd34
Rohith <119583656+Rohithchowk@users.noreply.github.com>
2024-02-18 21:49:59 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://3.110.48.22:5000", // Replace with your backend server URL
+      target: "https://kreedacbit.onrender.com", // Replace with your backend server URL
       changeOrigin: true,
     })
   );
